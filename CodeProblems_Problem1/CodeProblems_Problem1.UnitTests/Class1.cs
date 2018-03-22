4589fa10940c9840d2939a3608d9e388387fb301
Dwayne Green <dgreen@1800contacts.com>
2018-03-21 19:05:05 -0600

    Adding Project Folders

---
+++ CodeProblems_Problem1/CodeProblems_Problem1.UnitTests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CodeProblems_Problem1.UnitTests
+{
+    public class Class1
+    {
+    }
+}
---
+++ CodeProblems_Problem1/CodeProblems_Problem1/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CodeProblems_Problem1
+{
+    public class Class1
+    {
+    }
+}
